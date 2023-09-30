ff6a3c30a2b95113947015b43fbe9f2d435fc0e6
Al Ameen AR <amrameen769@gmail.com>
2023-09-30 16:10:35 +0530

    Button component updated with dynamic styling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,44 @@
 "use client"
 import Image from "next/image";
-import {FC} from "react";
+import React, {ButtonHTMLAttributes, FC} from "react";
+import Link from "next/link";
+import {cn} from "@/utils/tailwindUtils";
+import {className} from "postcss-selector-parser";
 
-interface ButtonPropTypes {
+type ButtonPropTypes = {
     icon: string
     text: string
+    customStyles?: string
+} & (buttonProps | linkProps)
+
+interface buttonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     variant: "primary" | "secondary"
 }
 
-const Button: FC<ButtonPropTypes> = ({icon, text, variant}) => {
-    return (
-        <button className={"flex justify-between items-center  text-white bg-[#0B83CC] rounded-full w-[220px] h-[56px] pl-5 pr-3 shadow-[#0B83CC] hover:bg-opacity-50 transition-all ease-in hover:text-black"}>
-            <span className={"font-medium text-[14px]"}>{text}</span>
+interface linkProps {
+    variant: "link"
+    link: string
+}
+
+const Button: FC<ButtonPropTypes> = (props) => {
+    const elementStyles = {
+        "bg-[#0B83CC] shadow-[0px_0px_21px_7px_rgba(11,131,204,0.39)]": props.variant === "primary" || props.variant === 'link',
+        "bg-[#F78251] shadow-[0px_0px_21px_7px_rgba(255,176,132,0.7)]": props.variant === "secondary"
+    }
+    return props.variant === 'link' ? (
+        <Link href={props.link}
+              className={cn("flex justify-between items-center text-white rounded-full w-[220px] h-[56px] pl-5 pr-3 hover:bg-opacity-50 transition-all ease-in hover:text-black", props.customStyles, elementStyles)}>
+            <span className={"font-medium text-[14px]"}>{props.text}</span>
             <div className={"relative h-[44px] w-[44px]"}>
-                <Image src={icon} alt={"button-icon"} fill className={"object-contain"}/>
+                <Image src={props.icon} alt={"button-icon"} fill className={"object-contain"}/>
+            </div>
+        </Link>
+    ) : (
+        <button type={props.type}
+                className={cn("flex justify-between items-center  text-white bg-[#0B83CC] rounded-full w-[220px] h-[56px] pl-5 pr-3 hover:bg-opacity-50 transition-all ease-in hover:text-black", props.customStyles, elementStyles)}>
+            <span className={"font-medium text-[14px]"}>{props.text}</span>
+            <div className={"relative h-[44px] w-[44px]"}>
+                <Image src={props.icon} alt={"button-icon"} fill className={"object-contain"}/>
             </div>
         </button>
     );
